f35a84d345fedd9e42bd8fadb630b2fc0e9c8992
Rodrigo Alcino <rodrigo_alcino@hotmail.com>
2021-09-10 22:00:16 -0300

    upd

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,69 @@
 import { render } from 'solid-js/web'
-import { createSignal, createMemo, createEffect, oncCleanUp } from 'solid-js'
+import { createSignal, createResource, For, createMemo, createEffect, onCleanup, mergeProps, onMount, Switch } from 'solid-js'
 import './index.css'
 
+const fetchData = () => {
+return fetch('https://rickandmortyapi.com/api/character')
+    .then(res => res.json())
+    .then(resp => {
+        console.log(resp.results)
+        return resp.results
+    })
+}   
+
 const App = () => {
+    const [getResults, setResults] = createSignal([])
+
+    const [getData, { mutate, refetch }] = createResource(fetchData, { initialValue: [] })
+
+    return (
+        <>
+        <h1>New app, with Solid.JS and Rick and Morty  API REST  :D</h1>
+        <Switch fallback={<div>Loading...</div>}>
+        <Match when={getData.error}>
+            <div>Error.</div>
+        </Match>
+
+        <Match when={getData.loading}>
+            <div>Loading...</div>
+        </Match>
+
+{/*         <Show when={getData.loading}>
+            <div>Loading...</div>
+            </Show>
+            
+            <Show when={getData.error}>
+            <div>Error.</div>
+        </Show> */}
+        <Match when={getData}>
+        <For each={getData()} /* fallback={<div>Loading...</div>} */>
+            {e => (
+                <div>
+                <span>{e.name}{<br/>}</span>
+                <span>{e.status}{<br/>}</span>
+                <img src={e.image} />
+                {<hr/>}
+            </div>                
+            )}
+        </For>
+        </Match>
+            </Switch>
+{/*         {
+            getData().map(e => {
+                return (
+                    <>
+                        <span>{e.name}</span>
+                        <span>{e.status}</span>
+                        <img src={e.image} />
+                    </>
+                )
+            })
+        } */}
+        </>
+    )
+}
+
+/* const App = () => {
     const [getCounter, setCounter] = createSignal(0);
 
     createEffect(prev => {
@@ -21,7 +82,7 @@ const App = () => {
         }}>Increment</button>
         </>
     )
-}
+} */
 
 render(App, document.getElementById('root'))
 /* render(App, document.body) */
